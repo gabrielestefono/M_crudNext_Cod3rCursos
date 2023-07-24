e9580661585cd4bc88dc4dbe6375561ca55e8dff
gabrielestefono <gabrielestefono@hotmail.com>
2023-07-24 01:14:36 -0300

    Componente Tabela

--- src/components/Tabela.tsx
+++ src/components/Tabela.tsx
@@ -5,14 +5,34 @@ interface TabelaProps{
 }
 
 export default function Tabela(props: TabelaProps){
-    return(
-        <table>
+    function renderizarCabecalho(){
+        return (
             <tr>
                 <th>Código </th>
                 <th>Nome</th>
                 <th>Idade</th>
             </tr>
-            
+        )
+        }
+    function renderizarDados(){
+        return props.clientes?.map((cliente, i)=>{
+            return(
+                <tr key={cliente.id}>
+                    <td>{cliente.id}</td>
+                    <td>{cliente.nome}</td>
+                    <td>{cliente.idade}</td>
+                </tr>
+            )
+        })
+    }
+    return(
+        <table>
+            <thead>
+                {renderizarCabecalho()}
+            </thead>
+            <tbody>
+                {renderizarDados()}
+            </tbody>
         </table>
     )
 }
